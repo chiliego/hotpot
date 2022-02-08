bbea00ef6b1b303316d2a4e46c01c6ffafe16c67
Huu Chi Vu <chiliego.github@gmail.com>
2022-02-08 12:38:17 +0000

    Using package org.hotpot instead of fsu, auto-loading watchable path from config file, config file is watchable

---
+++ agent/src/main/java/org/hotpot/agent/events/TransformerWatchEventHandler.java
@@ -0,0 +1,18 @@
+package org.hotpot.agent.events;
+
+import java.nio.file.Path;
+
+import org.hotpot.instrumentation.TransformerService;
+
+public class TransformerWatchEventHandler implements PathWatchEventHandler {
+    private TransformerService ts;
+
+    public TransformerWatchEventHandler(TransformerService transformerService) {
+        this.ts = transformerService;
+    }
+
+    @Override
+    public void handleModified(Path classFile) {
+        ts.swapClassFile(classFile);
+    }
+}
