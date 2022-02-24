d210ceaec96bfefdb246216021ce2083b9476ff0
Huu Chi Vu <chiliego.github@gmail.com>
2022-02-24 15:03:49 +0000

    Add new class also works with Tomcat

--- agent/src/main/java/org/hotpot/instrumentation/transformer/HotPotClassTransformer.java
+++ agent/src/main/java/org/hotpot/instrumentation/transformer/HotPotClassTransformer.java
@@ -13,7 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
@@ -28,7 +27,6 @@ public class HotPotClassTransformer implements ClassFileTransformer {
     private Map<String, byte[]> redefineClasses;
     private Supplier<Path> confFilePathSupplier;
     private List<String> redefinedClassloaders;
-    private Consumer<Class<?>[]> retransformFunc;
     private BiConsumer<Class<?>, byte[]> redefineFunc;
 
     public HotPotClassTransformer() {
@@ -40,12 +38,12 @@ public class HotPotClassTransformer implements ClassFileTransformer {
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
 
-        if (!isRedefined(loader)) {
-            redefine(loader);
-        }
-
         byte[] modifiedBytecode = getByteCode(className);
         if (modifiedBytecode != null) {
+            if (!isRedefined(loader)) {
+                redefine(loader, "loadClass", String.class, boolean.class);
+            }
+
             try {
                 boolean onlyChangeMethodBody = ASMUtils.onlyChangeMethodBody(classfileBuffer, modifiedBytecode);
                 if (onlyChangeMethodBody) {
@@ -57,8 +55,6 @@ public class HotPotClassTransformer implements ClassFileTransformer {
             } finally {
                 removeByteCode(className);
             }
-        } else {
-            LOGGER.error("No bytecode found for class [{}].", className);
         }
 
         return null;
@@ -66,10 +62,6 @@ public class HotPotClassTransformer implements ClassFileTransformer {
 
     public void setConfFilePath(Supplier<Path> supplier) {
         this.confFilePathSupplier = supplier;
-    }
-
-    public void setRetransformFunc(Consumer<Class<?>[]> retransformFunc) {
-        this.retransformFunc = retransformFunc;
     }
 
     public void setRedefineFunc(BiConsumer<Class<?>, byte[]> redefineFunc) {
@@ -85,9 +77,8 @@ public class HotPotClassTransformer implements ClassFileTransformer {
             e.printStackTrace();
         }
     }
-    
+
     public void addModifiedClass(String className, byte[] modifiedBytecode) {
-        LOGGER.info("loader [{}]", getClass().getClassLoader().getClass().getName());
         redefineClasses.put(internalName(className), modifiedBytecode);
         LOGGER.info("Add class [{}] from file [{}] to redefine List.", className);
     }
@@ -105,73 +96,74 @@ public class HotPotClassTransformer implements ClassFileTransformer {
     }
 
     private boolean isRedefined(ClassLoader loader) {
-        return redefinedClassloaders.contains(loader.getClass().getName());
+        String name = internalName(loader.getClass().getName());
+
+        boolean contains = redefinedClassloaders.contains(name);
+        return contains;
     }
 
     private void setAsRedefined(ClassLoader loader) {
-        redefinedClassloaders.add(loader.getClass().getName());
-    }
-
-    private byte[] redefine(ClassLoader loader) {
-        ClassLoader parentClassLoader = loader.getParent();
-        String loaderClassName = loader.getClass().getName();
-        try(InputStream byteCodeIS = parentClassLoader.getResourceAsStream(internalName(loaderClassName) + ".class")) {
-            return redefine(loader, byteCodeIS, "loadClass", String.class, boolean.class);
-        } catch(IOException e) {
-            LOGGER.error("Could not close class input stream for [{}]", loader.getClass().getName());
+        String name = internalName(loader.getClass().getName());
+        if (!redefinedClassloaders.contains(name)) {
+            redefinedClassloaders.add(name);
         }
-
-        return null;
     }
 
-    private byte[] redefine(ClassLoader classloader, InputStream byteCodeIS, String methodName, Class<?>... parameterTypes) {
-        Class<? extends ClassLoader> classloaderClass = classloader.getClass();
+    private boolean redefine(ClassLoader loader, String methodName, Class<?>... parameterTypes) {
         try {
+            Class<? extends ClassLoader> classloaderClass = loader.getClass();
             Method declaredMethod = classloaderClass.getDeclaredMethod(methodName, parameterTypes);
-            return redefine(classloader, byteCodeIS, declaredMethod);
+            boolean redefined = redefine(loader, declaredMethod);
+            if (redefined) {
+                setAsRedefined(loader);
+            }
+            return redefined;
         } catch (NoSuchMethodException e) {
-            ClassLoader parentClassloader = classloader.getParent();
-            if (parentClassloader != null) {
-                LOGGER.info("Try get method {}({}) from parent classloader [{}] of [{}]", methodName, parameterTypes,
-                        parentClassloader.getName(), classloader.getName());
-                return redefine(parentClassloader, byteCodeIS, methodName, parameterTypes);
+            ClassLoader parentClassLoader = loader.getParent();
+            if (parentClassLoader != null) {
+                LOGGER.info("redefine parent [{}] of [{}].", parentClassLoader.getClass().getName(),
+                        loader.getClass().getName());
+                boolean redefined = redefine(parentClassLoader, methodName, parameterTypes);
+                if (redefined) {
+                    setAsRedefined(parentClassLoader);
+                }
+                return redefined;
             }
         } catch (SecurityException e) {
-            LOGGER.error("In class [{}] exist no method {}({}).", classloader.getName(), methodName, parameterTypes);
+            LOGGER.error("In class [{}] exist no method {}({}).", loader.getName(), methodName, parameterTypes);
             e.printStackTrace();
         }
 
-        return null;
+        return false;
     }
 
-    private byte[] redefine(ClassLoader classloader, InputStream byteCodeIS, Method method) {
-        LOGGER.info("Redefine method {}({}) in classloader [{}].", classloader.getClass(), method.getName(),
-                method.getParameterTypes());
+    private boolean redefine(ClassLoader loader, Method method) {
+        ClassLoader parentClassLoader = loader.getParent();
+        String loaderClassName = loader.getClass().getName();
 
-        if (confFilePathSupplier != null) {
-            String methodName = method.getName();
-            String descriptor = ASMUtils.getMethodDescriptor(method);
-            String classPathConfFile = confFilePathSupplier.get().toAbsolutePath().toString();
-            UnaryOperator<ClassVisitor> claddloaderAdapter = cv -> new ClassloaderAdapter(cv, methodName, descriptor,
-                    classPathConfFile);
+        try (InputStream byteCodeIS = parentClassLoader
+                .getResourceAsStream(internalName(loaderClassName) + ".class")) {
+            if (confFilePathSupplier != null) {
+                String methodName = method.getName();
+                String descriptor = ASMUtils.getMethodDescriptor(method);
+                String classPathConfFile = confFilePathSupplier.get().toAbsolutePath().toString();
+                UnaryOperator<ClassVisitor> claddloaderAdapter = cv -> new ClassloaderAdapter(cv, methodName,
+                        descriptor,
+                        classPathConfFile);
 
-            byte[] redefinedByteCode = ASMUtils.applyClassVisitor(byteCodeIS, claddloaderAdapter, false);
-            addModifiedClass(classloader.getClass().getName(), redefinedByteCode);
-            LOGGER.info("Redefine classloader [{}] done.", classloader.getClass().getName());
-            setAsRedefined(classloader);
-            //retransform(classloader.getClass());
-            redefine(classloader.getClass(), redefinedByteCode);
-                    
-            return redefinedByteCode;
+                byte[] redefinedByteCode = ASMUtils.applyClassVisitor(byteCodeIS, claddloaderAdapter, false);
+                if (redefinedByteCode != null) {
+                    redefine(loader.getClass(), redefinedByteCode);
+                    LOGGER.info("Redefine classloader [{}] done.", loader.getClass().getName());
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("Could not close class input stream for [{}]", loader.getClass().getName());
         }
 
-        return null;
-    }
-
-    private void retransform(Class<?>... classes) {
-        if (retransformFunc != null) {
-            retransformFunc.accept(classes);
-        }
+        LOGGER.info("Redefine classloader [{}] failed.", loader.getClass().getName());
+        return false;
     }
 
     private void redefine(Class<?> clazz, byte[] byteCode) {
